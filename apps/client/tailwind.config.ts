27ddb2aa9fe5184e2e02fb772c11d2ce5365fd44
Phi Long <tranphilong030201@gmail.com>
2025-11-04 18:05:58 +0700

    Initialize project

---
+++ apps/client/tailwind.config.ts
@@ -0,0 +1,15 @@
+const config = {
+  prefix: 'tw-',
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+};
+export default config;
